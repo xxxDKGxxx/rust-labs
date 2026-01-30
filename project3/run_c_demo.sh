164146367501daf79687470fb21d29bee5d81bec
xxxDKGxxx <dominikzielinski678@gmail.com>
2026-01-30 16:25:08 +0100

    Add address sanitizer

--- project3/run_c_demo.sh
+++ project3/run_c_demo.sh
@@ -1,2 +1,2 @@
-gcc examples/c_demo.c -L ./target/release -lred_black_tree_dict -Wl,-rpath,./target/release -o c_demo
+gcc -fsanitize=address -g examples/c_demo.c -L ./target/release -lred_black_tree_dict -Wl,-rpath,./target/release -o c_demo
 ./c_demo
